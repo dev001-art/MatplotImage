1a4357bf755d6b2379bcba5d3ae5dd414b25e2c5
Harshit Chauhan <64789865+dev001-art@users.noreply.github.com>
2021-04-10 15:45:39 +0530

    Update reduce-colors.py

--- reduce-colors.py
+++ reduce-colors.py
@@ -2,8 +2,8 @@ import numpy as np
 from skimage import io
 from sklearn.cluster import KMeans
 
-original = io.imread('Random/qw.jpeg')
-n_colors = 20
+original = io.imread('path to image') # path to image
+n_colors = 20 # Number of colors in final image 
 
 arr = original.reshape((-1, 3))
 kmeans = KMeans(n_clusters=n_colors, random_state=42).fit(arr)
@@ -11,4 +11,4 @@ labels = kmeans.labels_
 centers = kmeans.cluster_centers_
 less_colors = centers[labels].reshape(original.shape).astype('uint8')
 
-io.imsave('Random/qwerty.png',less_colors)
+io.imsave('output path',less_colors)
